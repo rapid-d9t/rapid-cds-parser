ff918c822a3fe4aa55c6349038dd97c27b99b562
zkud <kudirko.anton@gmail.com>
2021-06-03 13:52:26 +0300

    Fixed imports

--- src/parser/mod.rs
+++ src/parser/mod.rs
@@ -1,2 +1,4 @@
-pub mod ast;
+mod ast;
 pub mod parser;
+use lalrpop_util::lalrpop_mod;
+lalrpop_mod!(cds, "/parser/cds.rs");
--- src/parser/parser.rs
+++ src/parser/parser.rs
@@ -3,9 +3,6 @@ use std::io::prelude::*;
 use std::fs::File;
 use std::path::Path;
 
-use lalrpop_util::lalrpop_mod;
-lalrpop_mod!(pub cds);
-
 use super::ast::traits::ast_term::ASTTerm;
 
 pub struct Parser {
@@ -25,7 +22,7 @@ impl Parser {
         let mut content = String::new();
         file.read_to_string(&mut content).unwrap();
 
-        let module = cds::ModuleParser::new().parse(&content).unwrap();
+        let module = super::cds::ModuleParser::new().parse(&content).unwrap();
 
         module.convert_to_json()
     }
