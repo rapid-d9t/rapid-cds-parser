be6d5bfd34b7d35a6c20aea7b40308a58231a753
zkud <kudirko.anton@gmail.com>
2021-06-09 17:21:27 +0300

    Implemented a pretty simple unit-test

--- src/parser/parser.rs
+++ src/parser/parser.rs
@@ -37,3 +37,47 @@ impl Parser {
         Ok(module)
     }
 }
+
+#[cfg(test)]
+mod tests {
+    use std::fs::remove_file;
+    use std::fs::File;
+    use std::io::prelude::*;
+
+    use super::Parser;
+
+    #[test]
+    fn with_correct_input_it_translates() {
+        let mut test_file = File::create("test.cds").unwrap();
+        test_file
+            .write_all(
+                b"
+            entity Test {
+                field : TestType;
+                field2    : TestType2;
+            }
+        
+            service TestService {
+                type test : Test;
+        
+                entity Test2 : Aspect1 {
+                field3 : Test3;
+                }
+        
+                action atest(arg1: Test);
+                action atest1(arg1: Test) returns Test;
+                action atest2(arg1: Test) returns array of Test;
+        
+                function ftest0() returns Test;
+                function ftest1(arg1: Test) returns Test;
+                function ftest2(arg1: Test) returns array of Test;
+            }
+        ",
+            )
+            .unwrap();
+
+        Parser::new("test.cds".to_string()).parse().unwrap();
+
+        remove_file("test.cds").unwrap();
+    }
+}
