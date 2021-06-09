627800d1119b7811589ea099a93732d6d6039307
zkud <kudirko.anton@gmail.com>
2021-06-09 17:51:45 +0300

    Provided more parser tests

--- src/parser/parser.rs
+++ src/parser/parser.rs
@@ -48,36 +48,52 @@ mod tests {
 
     #[test]
     fn with_correct_input_it_translates() {
-        let mut test_file = File::create("test.cds").unwrap();
+        let mut test_file = File::create("test_correct.cds").unwrap();
         test_file
             .write_all(
                 b"
-            entity Test {
-                field : TestType;
-                field2    : TestType2;
-            }
-        
-            service TestService {
-                type test : Test;
-        
-                entity Test2 : Aspect1 {
-                field3 : Test3;
-                }
-        
-                action atest(arg1: Test);
-                action atest1(arg1: Test) returns Test;
-                action atest2(arg1: Test) returns array of Test;
-        
-                function ftest0() returns Test;
-                function ftest1(arg1: Test) returns Test;
-                function ftest2(arg1: Test) returns array of Test;
-            }
-        ",
+                    service TestService {
+                        entity Test2 : Aspect1 {
+                            field3 : Test3;
+                        }
+                    }
+                ",
             )
             .unwrap();
 
-        Parser::new("test.cds".to_string()).parse().unwrap();
+        let result = Parser::new("test_correct.cds".to_string()).parse();
 
-        remove_file("test.cds").unwrap();
+        remove_file("test_correct.cds").unwrap();
+
+        assert!(result.is_ok());
+    }
+
+    #[test]
+    fn with_unexisting_file_it_fails() {
+        let result = Parser::new("test.cds".to_string()).parse();
+
+        assert!(result.is_err());
+    }
+
+    #[test]
+    fn with_syntactically_incorrect_if_fails() {
+        let mut test_file = File::create("test_incorrect.cds").unwrap();
+        test_file
+            .write_all(
+                b"
+                    service TestService {
+                        function ftest0() returns Test;
+                        function ftest1(turns Test;
+                        function ftest2(arg1: Test) returns array of Test;
+                    }
+                ",
+            )
+            .unwrap();
+
+        let result = Parser::new("test_incorrect.cds".to_string()).parse();
+
+        remove_file("test_incorrect.cds").unwrap();
+
+        assert!(result.is_err());
     }
 }
