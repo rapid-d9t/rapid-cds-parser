8f47e46ecdc29d2d74c1b153fd7327d54de0296e
zkud <kudirko.anton@gmail.com>
2021-06-10 23:06:37 +0300

    Added unit tests for parse_error

--- src/parser/parse_error.rs
+++ src/parser/parse_error.rs
@@ -54,3 +54,30 @@ impl From<io::Error> for ParseError {
         ParseError::new(format!("{}", error), ParseErrorType::FileIOError)
     }
 }
+
+#[cfg(test)]
+mod tests {
+    use super::ParseError;
+    use super::ParseErrorType;
+
+    #[test]
+    fn it_inits() {
+        let file_error = ParseError::new("file error".to_string(), ParseErrorType::FileIOError);
+        let syntax_error = ParseError::new("syntax error".to_string(), ParseErrorType::SyntaxError);
+
+        assert_eq!(file_error.get_message(), "file error");
+        assert_eq!(file_error.get_error_type(), ParseErrorType::FileIOError);
+
+        assert_eq!(syntax_error.get_message(), "syntax error");
+        assert_eq!(syntax_error.get_error_type(), ParseErrorType::SyntaxError);
+    }
+
+    #[test]
+    fn it_displayable() {
+        let file_error = ParseError::new("file error".to_string(), ParseErrorType::FileIOError);
+        let syntax_error = ParseError::new("syntax error".to_string(), ParseErrorType::SyntaxError);
+
+        assert_eq!(format!("{}", file_error), "parse error FileIOError, reason: file error");
+        assert_eq!(format!("{}", syntax_error), "parse error SyntaxError, reason: syntax error");
+    }
+}
