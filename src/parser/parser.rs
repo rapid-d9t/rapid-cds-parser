08c5e9afee27ae7a1d4d40f0006e38a434855d3c
zkud <kudirko.anton@gmail.com>
2021-06-10 22:27:37 +0300

    Enhanced parser tests

--- src/parser/parser.rs
+++ src/parser/parser.rs
@@ -56,11 +56,33 @@ mod tests {
         test_file
             .write_all(
                 b"
-                    service TestService {
-                        entity Test2 : Aspect1 {
-                            field3 : Test3;
-                        }
+                type test : Testtype;
+
+                entity Test {
+                    field : TestType;
+                    field2    : TestType2;
+                }
+
+                entity Test2 : Astpect {
+                    field : TestType;
+                    field2    : TestType2;
+                }
+            
+                service TestService {
+                    type test : Test;
+            
+                    entity Test2 : Aspect1 {
+                    field3 : Test3;
                     }
+            
+                    action atest(arg1: Test);
+                    action atest1(arg1: Test) returns Test;
+                    action atest2(arg1: Test) returns array of Test;
+            
+                    function ftest0() returns Test;
+                    function ftest1(arg1: Test) returns Test;
+                    function ftest2(arg1: Test) returns array of Test;
+                }
                 ",
             )
             .unwrap();
