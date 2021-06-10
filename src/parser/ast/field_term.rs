e87bc547cbbf9efe731d2df5926ad5e91753c8d6
zkud <kudirko.anton@gmail.com>
2021-06-10 22:56:03 +0300

    Removed unused code

--- src/parser/ast/field_term.rs
+++ src/parser/ast/field_term.rs
@@ -12,14 +12,6 @@ impl FieldTerm {
     pub fn new(name: NameTerm, type_name: NameTerm) -> FieldTerm {
         FieldTerm { name, type_name }
     }
-
-    pub fn get_name(&self) -> String {
-        self.name.get_value()
-    }
-
-    pub fn get_type_name(&self) -> String {
-        self.type_name.get_value()
-    }
 }
 
 impl ASTTerm for FieldTerm {
