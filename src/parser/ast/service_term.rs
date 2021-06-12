49e26525f6e4c91a25fcda7547a630e0fd77f06b
zkud <kudirko.anton@gmail.com>
2021-06-12 21:37:29 +0300

    Enhanced service tests

--- src/parser/ast/service_term.rs
+++ src/parser/ast/service_term.rs
@@ -77,6 +77,10 @@ impl ASTTerm for ServiceTerm {
 
 #[cfg(test)]
 mod tests {
+    use crate::parser::ast::traits::ast_term::ASTTerm;
+
+    use crate::ir::ir_component::IRComponent;
+
     use super::ServiceTerm;
     use crate::parser::ast::entity_term::EntityTerm;
     use crate::parser::ast::name_term::NameTerm;
@@ -89,9 +93,44 @@ mod tests {
         let service_name = NameTerm::new_boxed("Test".to_string());
         let entity_name = NameTerm::new_boxed("TestEntity".to_string());
         let entity = EntityTerm::new_boxed(entity_name, Vec::new(), Vec::new());
-        let term = ServiceTerm::new(service_name, vec![entity]);
-        let term: Box<dyn ModuleUsableTerm> = Box::new(term);
+        let term: Box<dyn ModuleUsableTerm> = ServiceTerm::new_boxed(service_name, vec![entity]);
 
         assert_eq!(term.get_type(), ModuleTermType::Service);
+    }
+
+    #[test]
+    fn it_generates_ir() {
+        let service_name = NameTerm::new_boxed("Test".to_string());
+        let entity_name = NameTerm::new_boxed("TestEntity".to_string());
+        let entity = EntityTerm::new_boxed(entity_name, Vec::new(), Vec::new());
+        let service_term = ServiceTerm::new_boxed(service_name, vec![entity]);
+        let service_ir = service_term.generate_ir();
+
+        let entity_fields = vec![
+            (
+                "name",
+                Box::new(IRComponent::new_string("TestEntity".to_string())),
+            ),
+            ("fields", Box::new(IRComponent::new_array(vec![]))),
+            ("aspects", Box::new(IRComponent::new_array(vec![]))),
+            ("isProjection", Box::new(IRComponent::new_bool(false))),
+        ];
+        let correct_ir_mock_fields = vec![
+            (
+                "name",
+                Box::new(IRComponent::new_string("Test".to_string())),
+            ),
+            (
+                "entities",
+                Box::new(IRComponent::new_array(vec![Box::new(
+                    IRComponent::new_object_from_vec(entity_fields),
+                )])),
+            ),
+            ("actions", Box::new(IRComponent::new_array(vec![]))),
+            ("functions", Box::new(IRComponent::new_array(vec![]))),
+        ];
+        let correct_ir = IRComponent::new_object_from_vec(correct_ir_mock_fields);
+
+        assert_eq!(service_ir, Box::new(correct_ir));
     }
 }
