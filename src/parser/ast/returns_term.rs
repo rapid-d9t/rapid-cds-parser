cee693a298570fa79a13837bba71f93bb312bae1
zkud <kudirko.anton@gmail.com>
2021-06-12 17:42:46 +0300

    Added unit tests for returns term

--- src/parser/ast/returns_term.rs
+++ src/parser/ast/returns_term.rs
@@ -43,3 +43,46 @@ impl ASTTerm for ReturnsTerm {
         Box::new(IRComponent::new_object_from_map(fields))
     }
 }
+
+#[cfg(test)]
+mod tests {
+    use super::ReturnsTerm;
+    use crate::ir::ir_component::IRComponent;
+    use crate::parser::ast::name_term::NameTerm;
+    use crate::parser::ast::traits::ast_term::ASTTerm;
+
+    #[test]
+    fn it_generates_ir() {
+        let returns_term = ReturnsTerm::new_boxed(NameTerm::new_boxed("mock-type".to_string()));
+        let returns_ir = returns_term.generate_ir();
+
+        let correct_ir_mock_returns = vec![
+            (
+                "type",
+                Box::new(IRComponent::new_string("mock-type".to_string())),
+            ),
+            ("isArrayed", Box::new(IRComponent::new_bool(false))),
+        ];
+        let correct_ir = IRComponent::new_object_from_vec(correct_ir_mock_returns);
+
+        assert_eq!(returns_ir, Box::new(correct_ir));
+    }
+
+    #[test]
+    fn with_arrayed_it_generates_ir() {
+        let returns_term =
+            ReturnsTerm::new_arrayed_boxed(NameTerm::new_boxed("mock-type".to_string()));
+        let returns_ir = returns_term.generate_ir();
+
+        let correct_ir_mock_returns = vec![
+            (
+                "type",
+                Box::new(IRComponent::new_string("mock-type".to_string())),
+            ),
+            ("isArrayed", Box::new(IRComponent::new_bool(true))),
+        ];
+        let correct_ir = IRComponent::new_object_from_vec(correct_ir_mock_returns);
+
+        assert_eq!(returns_ir, Box::new(correct_ir));
+    }
+}
