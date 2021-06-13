3fd593ed1197b3dd191610d9ff50029c2c854f3c
zkud <kudirko.anton@gmail.com>
2021-06-13 17:25:03 +0300

    Refactored function term

--- src/parser/ast/function_term.rs
+++ src/parser/ast/function_term.rs
@@ -2,7 +2,6 @@ use super::traits::ast_term::ASTTerm;
 use super::traits::service_term_type::ServiceTermType;
 use super::traits::service_usable_term::ServiceUsableTerm;
 use crate::ir::ir_component::IRComponent;
-use std::collections::HashMap;
 
 pub struct FunctionTerm {
     name: Box<dyn ASTTerm>,
@@ -10,7 +9,35 @@ pub struct FunctionTerm {
     returned_type: Box<dyn ASTTerm>,
 }
 
+impl ServiceUsableTerm for FunctionTerm {
+    fn get_type(&self) -> ServiceTermType {
+        ServiceTermType::Function
+    }
+}
+
+impl ASTTerm for FunctionTerm {
+    fn generate_ir(&self) -> Box<IRComponent> {
+        let fields = vec![
+            ("name", self.name.generate_ir()),
+            ("params", self.generate_params_ir()),
+            ("output", self.returned_type.generate_ir()),
+        ];
+
+        Box::new(IRComponent::new_object_from_vec(fields))
+    }
+}
+
 impl FunctionTerm {
+    fn generate_params_ir(&self) -> Box<IRComponent> {
+        let params = self
+            .params
+            .iter()
+            .map(|param| param.generate_ir())
+            .collect();
+
+        Box::new(IRComponent::new_array(params))
+    }
+
     pub fn new_boxed(
         name: Box<dyn ASTTerm>,
         params: Vec<Box<dyn ASTTerm>>,
@@ -29,32 +56,6 @@ impl FunctionTerm {
             params,
             returned_type,
         }
-    }
-}
-
-impl ServiceUsableTerm for FunctionTerm {
-    fn get_type(&self) -> ServiceTermType {
-        ServiceTermType::Function
-    }
-}
-
-impl ASTTerm for FunctionTerm {
-    fn generate_ir(&self) -> Box<IRComponent> {
-        let params = self
-            .params
-            .iter()
-            .map(|param| param.generate_ir())
-            .collect();
-
-        let mut fields = HashMap::<String, Box<IRComponent>>::new();
-        fields.insert("name".to_string(), self.name.generate_ir());
-        fields.insert(
-            "params".to_string(),
-            Box::new(IRComponent::new_array(params)),
-        );
-        fields.insert("output".to_string(), self.returned_type.generate_ir());
-
-        Box::new(IRComponent::new_object_from_map(fields))
     }
 }
 
