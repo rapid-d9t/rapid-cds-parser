ebad5d08d5d7047c6400f2b54a524706f5f7b502
zkud <kudirko.anton@gmail.com>
2021-06-13 16:35:30 +0300

    Refactored action term

--- src/parser/ast/action_term.rs
+++ src/parser/ast/action_term.rs
@@ -2,7 +2,6 @@ use super::traits::ast_term::ASTTerm;
 use super::traits::service_term_type::ServiceTermType;
 use super::traits::service_usable_term::ServiceUsableTerm;
 use crate::ir::ir_component::IRComponent;
-use std::collections::HashMap;
 
 pub struct ActionTerm {
     name: Box<dyn ASTTerm>,
@@ -18,39 +17,38 @@ impl ServiceUsableTerm for ActionTerm {
 
 impl ASTTerm for ActionTerm {
     fn generate_ir(&self) -> Box<IRComponent> {
+        let mut fields = vec![
+            ("name", self.name.generate_ir()),
+            ("params", self.generate_params_ir()),
+        ];
+
+        self.add_output_field(&mut fields);
+
+        Box::new(IRComponent::new_object_from_vec(fields))
+    }
+}
+
+impl ActionTerm {
+    fn generate_params_ir(&self) -> Box<IRComponent> {
         let params = self
             .params
             .iter()
             .map(|param| param.generate_ir())
             .collect();
 
-        let mut fields = HashMap::<String, Box<IRComponent>>::new();
-        fields.insert("name".to_string(), self.name.generate_ir());
-        fields.insert(
-            "params".to_string(),
-            Box::new(IRComponent::new_array(params)),
-        );
-        match &self.returned_type {
-            Some(returned_type) => {
-                fields.insert("output".to_string(), returned_type.generate_ir());
-                fields.insert(
-                    "hasOutput".to_string(),
-                    Box::new(IRComponent::new_bool(true)),
-                );
-            }
-            None => {
-                fields.insert(
-                    "hasOutput".to_string(),
-                    Box::new(IRComponent::new_bool(false)),
-                );
-            }
-        }
-
-        Box::new(IRComponent::new_object_from_map(fields))
+        Box::new(IRComponent::new_array(params))
     }
-}
 
-impl ActionTerm {
+    fn add_output_field(&self, fields: &mut Vec<(&str, Box<IRComponent>)>) {
+        if let Some(returned_type) = &self.returned_type {
+            fields.push(("output", returned_type.generate_ir()));
+            fields.push(("hasOutput", Box::new(IRComponent::new_bool(true))));
+        }
+        else {
+            fields.push(("hasOutput", Box::new(IRComponent::new_bool(false))));
+        }
+    }
+
     pub fn new_boxed(
         name: Box<dyn ASTTerm>,
         params: Vec<Box<dyn ASTTerm>>,
